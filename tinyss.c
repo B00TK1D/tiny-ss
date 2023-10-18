7ad2bb7dcb30bd4dba512145a42da9340452da0b
B00TK1D <stearns.josiah@gmail.com>
2023-10-17 23:05:21 -0400

    Added check for root privilidges

--- tinyss.c
+++ tinyss.c
@@ -12,12 +12,12 @@
 
 #define MAX_PORT 65535
 
-void scan(int sock_type, int max_port) {
+void scan(int sock_type, int min_port, int max_port) {
     int sock;
     int ret;
     struct sockaddr_in addr;
     char first = 1;
-    for (int port = 1; port <= max_port; port++) {
+    for (int port = min_port; port <= max_port; port++) {
         sock = socket(AF_INET, sock_type, 0);
         if (sock < 0) {
             exit(1);
@@ -41,10 +41,33 @@ void scan(int sock_type, int max_port) {
     }
 }
 
-int main(int argc, char **argv) {
+int check_root() {
     int sock;
     int ret;
+    struct sockaddr_in addr;
+    for (int port = 1; port <= 5; port++) {
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            exit(1);
+        }
 
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = INADDR_ANY;
+
+        ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+        if (ret < 0) {
+            close(sock);
+            return 1024;
+        }
+
+        close(sock);
+    }
+    return 1;
+}
+
+int main(int argc, char **argv) {
+    int min_port = check_root();
     int max_port = MAX_PORT;
 
     if (argc > 1 && atoi(argv[argc - 1]) > 0 && atoi(argv[argc - 1]) <= MAX_PORT) {
@@ -53,17 +76,15 @@ int main(int argc, char **argv) {
 
     if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'j') {
         printf("{tcp:[");
-        scan(SOCK_STREAM, max_port);
+        scan(SOCK_STREAM, min_port, max_port);
         printf("],udp:[");
-        scan(SOCK_DGRAM, max_port);
+        scan(SOCK_DGRAM, min_port, max_port);
         printf("]}");
     } else {
-        scan(SOCK_STREAM, max_port);
+        scan(SOCK_STREAM, min_port, max_port);
         printf(";");
-        scan(SOCK_DGRAM, max_port);
+        scan(SOCK_DGRAM, min_port, max_port);
     }
-
-    
 
     return 0;
 }
